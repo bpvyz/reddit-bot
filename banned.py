caaf663146ac5a8e570e527cb70a87a24fc0c4e1
B <bogdanbokipaunovic@gmail.com>
2020-07-05 18:59:17 +0200

    fourteenth commit

--- banned.py
+++ banned.py
@@ -13,6 +13,7 @@ def json_dump(subreddit):
     with open('subreddits.json') as f:
         data = json.load(f)
         if subreddit not in data['disallowed']:
+            print(f"You've been banned from {subreddit}, appending to subreddits.json!")
             data['disallowed'].append(subreddit)
     with open('subreddits.json', 'w') as f:
         json.dump(data, f, sort_keys=True, indent=4)
